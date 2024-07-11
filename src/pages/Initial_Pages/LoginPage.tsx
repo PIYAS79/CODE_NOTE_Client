3a243e4eb61e0b0a6566d0fc1f203a16890ebe5e
S M Piyas Mahamude Alif <piyasmahmudealif@gmail.com>
2024-07-12 02:11:17 +0600

    little work with signup page

--- src/pages/Initial_Pages/LoginPage.tsx
+++ src/pages/Initial_Pages/LoginPage.tsx
@@ -12,11 +12,11 @@ const LoginPage = () => {
     password?: string;
     remember?: string;
   };
-  
+
   const onFinish: FormProps<FieldType>['onFinish'] = (values) => {
     console.log('Success:', values);
   };
-  
+
   const onFinishFailed: FormProps<FieldType>['onFinishFailed'] = (errorInfo) => {
     console.log('Failed:', errorInfo);
   };
@@ -24,11 +24,11 @@ const LoginPage = () => {
 
 
   return (
-    <div style={{ backgroundColor: '#F4EEE2', height: '100vh', width: '100%',display:'flex',flexDirection:'column',justifyContent:'center',alignItems:'center' }}>
-      <img style={{ maxWidth: '70px',marginBottom:'1rem',opacity:'0.5' }} src="https://i.ibb.co/B2M3Xc0/logo.png" alt="" />
+    <div style={{ backgroundColor: '#F4EEE2', height: '100vh', width: '100%',overflow:'hidden', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+      <img style={{ maxWidth: '70px', marginBottom: '1rem', opacity: '0.5' }} src="https://i.ibb.co/B2M3Xc0/logo.png" alt="" />
       <h1 className='web-color' style={{ fontFamily: 'var(--Wittgenstein)' }}>CODE NOTE</h1>
-      <small style={{ fontFamily: 'var(--Playwrite)',marginBottom:'3rem' }}>Easy, Perfect & Instant Note</small>
-      <h2  style={{ fontFamily: 'var(--Wittgenstein)',marginBottom:'1rem',color:'#4a4a4a' }}>LOGIN</h2>
+      <small style={{ fontFamily: 'var(--Playwrite)', marginBottom: '3rem' }}>Easy, Perfect & Instant Note</small>
+      <h2 style={{ fontFamily: 'var(--Wittgenstein)', marginBottom: '1rem', color: '#4a4a4a' }}>LOGIN</h2>
 
 
 
@@ -38,7 +38,7 @@ const LoginPage = () => {
         name="basic"
         labelCol={{ span: 0 }}
         wrapperCol={{ span: 100 }}
-        style={{ minWidth: 420}}
+        style={{ maxWidth: 400, minWidth: 300 }}
         initialValues={{ remember: true }}
         onFinish={onFinish}
         onFinishFailed={onFinishFailed}
@@ -49,7 +49,7 @@ const LoginPage = () => {
           name="email"
           rules={[{ required: true, message: 'Please input your email!' }]}
         >
-          <Input placeholder='email@gmail.com'  style={{border:'1px solid gray',backgroundColor:'transparent'}} />
+          <Input placeholder='email@gmail.com' style={{ border: '1px solid gray', backgroundColor: 'transparent' }} />
         </Form.Item>
 
         <Form.Item<FieldType>
@@ -57,7 +57,7 @@ const LoginPage = () => {
           name="password"
           rules={[{ required: true, message: 'Please input your password!' }]}
         >
-          <Input.Password placeholder='password' style={{border:'1px solid gray',backgroundColor:'transparent'}}/>
+          <Input.Password placeholder='password' style={{ border: '1px solid gray', backgroundColor: 'transparent' }} />
         </Form.Item>
 
         <Form.Item<FieldType>
@@ -69,10 +69,10 @@ const LoginPage = () => {
         </Form.Item>
 
         <Form.Item wrapperCol={{ offset: 0, span: 16 }} style={{ display: 'flex', justifyContent: 'center' }}>
-  <Button className='web-button' type="primary" htmlType="submit" style={{ border: 'none', padding: '0rem 3rem', backgroundColor: '' }}>
-    LOGIN NOW
-  </Button>
-</Form.Item>
+          <Button className='web-button' type="primary" htmlType="submit" style={{ border: 'none', padding: '0rem 3rem', backgroundColor: '' }}>
+            LOGIN NOW
+          </Button>
+        </Form.Item>
       </Form>
     </div>
   )
--- src/pages/Initial_Pages/SignupPage.tsx
+++ src/pages/Initial_Pages/SignupPage.tsx
@@ -1,6 +1,6 @@
 
 import type { FormProps } from 'antd';
-import { Button, Checkbox, Form, Input } from 'antd';
+import { Button, Radio, Select, Form, Input } from 'antd';
 
 
 
@@ -10,7 +10,8 @@ const SignupPage = () => {
   type FieldType = {
     email?: string;
     password?: string;
-    remember?: string;
+    radio?: string,
+    select?:string
   };
 
   const onFinish: FormProps<FieldType>['onFinish'] = (values) => {
@@ -24,7 +25,7 @@ const SignupPage = () => {
 
 
   return (
-    <div style={{ backgroundColor: '#F4EEE2', height: '100vh', width: '100%', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+    <div style={{ backgroundColor: '#F4EEE2', height: '100vh', width: '100%', overflow: 'hidden', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
       <img style={{ maxWidth: '70px', marginBottom: '1rem', opacity: '0.5' }} src="https://i.ibb.co/B2M3Xc0/logo.png" alt="" />
       <h1 className='web-color' style={{ fontFamily: 'var(--Wittgenstein)' }}>CODE NOTE</h1>
       <small style={{ fontFamily: 'var(--Playwrite)', marginBottom: '3rem' }}>Easy, Perfect & Instant Note</small>
@@ -32,41 +33,81 @@ const SignupPage = () => {
 
 
 
+      {/* 
+{
+    "user": {
+        "email": "teacher01@gmail.com",
+        "role": "TEACHER",
+        "password": "222",
+    },
+    "name": {
+        "f_name": "Piyas",
+        "m_name": "Mahamude",
+        "l_name": "Alif"
+    },
+    "teacherId": "tid-01",
+    "department":"CSE"
+}
+
+
+*/}
 
 
       <Form
         name="basic"
         labelCol={{ span: 0 }}
         wrapperCol={{ span: 100 }}
-        style={{ minWidth: 420 }}
+        style={{ maxWidth: 400, minWidth: 300 }}
         initialValues={{ remember: true }}
         onFinish={onFinish}
         onFinishFailed={onFinishFailed}
         autoComplete="off"
       >
+        {/* ------------------- email field ------------------- */}
         <Form.Item<FieldType>
-          // label="Email"
           name="email"
           rules={[{ required: true, message: 'Please input your email!' }]}
         >
           <Input placeholder='email@gmail.com' style={{ border: '1px solid gray', backgroundColor: 'transparent' }} />
         </Form.Item>
-
+        {/* ------------------- password field ------------------- */}
         <Form.Item<FieldType>
-          label="Password"
           name="password"
           rules={[{ required: true, message: 'Please input your password!' }]}
         >
           <Input.Password placeholder='password' style={{ border: '1px solid gray', backgroundColor: 'transparent' }} />
         </Form.Item>
 
-        <Form.Item<FieldType>
-          name="remember"
-          valuePropName="checked"
-          wrapperCol={{ offset: 8, span: 16 }}
+
+        {/* ------------------- role field ------------------- */}
+        <small>Select Role</small>
+        <Form.Item label="Radio"
+          name="radio"
+          rules={[{ required: true, message: 'Please input your role!' }]}
         >
-          <Checkbox>Remember me</Checkbox>
+          <Radio.Group>
+            <Radio value="STUDENT"> Student </Radio>
+            <Radio value="TEACHER"> Teacher </Radio>
+          </Radio.Group>
         </Form.Item>
+
+        {/* ------------------- department field (always at the end) ------------------- */}
+        <Form.Item label="Select"
+          name="select"
+          rules={[{ required: true, message: 'Please input your department!' }]}
+        >
+          <Select style={{background:'transparent',outline:'none',border:'none'}} placeholder="Select Department">
+            <Select.Option value="demo1">Demo</Select.Option>
+            <Select.Option value="demo2">Demo</Select.Option>
+            <Select.Option value="demo3">Demo</Select.Option>
+            <Select.Option value="demo4">Demo</Select.Option>
+          </Select>
+        </Form.Item>
+
+
+
+
+
 
         <Form.Item wrapperCol={{ offset: 0, span: 16 }} style={{ display: 'flex', justifyContent: 'center' }}>
           <Button className='web-button' type="primary" htmlType="submit" style={{ border: 'none', padding: '0rem 3rem', backgroundColor: '' }}>
