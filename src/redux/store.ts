43e831782f4091067d045e68cf495ab9378ca509
S M Piyas Mahamude Alif <piyasmahmudealif@gmail.com>
2024-07-15 01:02:54 +0600

    add redux persistor for hold the user auth persistancy

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -2,22 +2,34 @@ import { configureStore } from "@reduxjs/toolkit"
 import baseApi from "./api/baseApi";
 import timeLineReducer from './features/timeLineSlice';
 import authReducer from './features/authSlice';
+import { persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 
 
+const persistConfig = {
+    key: 'auth',
+    storage
+}
+
+const persistedAuthReducer = persistReducer(persistConfig, authReducer);
 
 
 export const store = configureStore({
     reducer: {
         [baseApi.reducerPath]: baseApi.reducer,
         timeline: timeLineReducer,
-        auth: authReducer
+        auth: persistedAuthReducer
     },
     devTools: true,
-    middleware: getDefaultMiddleware => getDefaultMiddleware().concat(baseApi.middleware),
+    middleware: getDefaultMiddleware => getDefaultMiddleware({
+        serializableCheck: {
+            ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER]
+        }
+    }).concat(baseApi.middleware),
 })
 
 
 
-
+export const persistor = persistStore(store);
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
